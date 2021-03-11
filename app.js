8e17b8d72c74ff044f395afb41634d251ab43f34
Akash Adhikary <61286006+akash-adhikary@users.noreply.github.com>
2021-03-11 18:00:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const sqlcon = require('./sql-con')
 const Cred = require('./DbCred')
-            const connection = new sqlcon(Cred['cred_1']);
-            const app = express()
-            const port = 3000
-            console.log(connection.getConCred());
-            connection.connect();
+            
+    const app = express()
+    const port = 3000
+    
+    const connection = new sqlcon(Cred['cred_1']);
+    console.log(connection.getConCred());
+    connection.connect();
 
-            app.get('/', (req, res) => {
-            res.send('Hello World!')
-            })
+    app.get('/', (req, res) => {
+    res.send('Hello World!')
+    })
 
-            app.listen(port, () => {
-                console.log(`Example app listening at http://localhost:${port}`)
-            });
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
 
             
